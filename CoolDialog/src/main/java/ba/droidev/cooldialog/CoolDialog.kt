da8e995614244d83243820a359a60a7785578ae9
Edin Hasanović <87568429+EdinHas26@users.noreply.github.com>
2021-09-20 21:59:26 +0200

    Initial commit

---
+++ CoolDialog/src/main/java/ba/droidev/cooldialog/CoolDialog.kt
@@ -0,0 +1,10 @@
+package ba.droidev.cooldialog
+
+import android.content.Context
+import android.view.View
+
+class CoolDialog(context: Context) : View(context) {
+
+    //Extend View, constructor and other methods to implement
+
+}
