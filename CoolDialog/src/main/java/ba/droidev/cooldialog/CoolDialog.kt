51cab03ce94af349aa7f569c30d86ace4e96e95b
Edin Hasanović <87568429+EdinHas26@users.noreply.github.com>
2021-09-21 22:12:13 +0200

    Final commit

--- CoolDialog/src/main/java/ba/droidev/cooldialog/CoolDialog.kt
+++ CoolDialog/src/main/java/ba/droidev/cooldialog/CoolDialog.kt
@@ -1,10 +1,192 @@
 package ba.droidev.cooldialog
 
+import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.view.marginTop
+import com.google.android.material.button.MaterialButton
+import java.lang.Exception
 
-class CoolDialog(context: Context) : View(context) {
+open class Dialog(context: Context)
+class CoolDialog(context: Context): Dialog(context) {
 
-    //Extend View, constructor and other methods to implement
+    //Override setContentView so its always set to R.layout.dialog_layout
+    override fun setContentView(layoutResID: Int) {
+        super.setContentView(R.layout.dialog_layout)
+    }
 
+    //Call this function to request content view and set it to R.layout.dialog_layout
+    fun requestContentView() {
+        setContentView(R.layout.dialog_layout)
+    }
+
+    //set image resource by resource id
+    fun setImageResource(id: Int) {
+        try {
+            this.findViewById<ImageView>(R.id.iv_circle_image).setImageResource(id)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    //get root layout of the dialog layout
+    fun getRootLayout() : RelativeLayout {
+        val relativeLayout: RelativeLayout = this.findViewById(R.id.rl_root)
+        return relativeLayout
+    }
+
+    //set text on the first TextView in the layout
+    fun setTextOnFirstTextView(text: String) {
+        this.findViewById<TextView>(R.id.tv_text1).setText(text)
+    }
+    //set text on the second TextView in the layout
+    fun setTextOnSecondTextView(text: String) {
+        this.findViewById<TextView>(R.id.tv_text2).setText(text)
+    }
+    //set text on the third TextView in the layout
+    fun setTextOnThirdTextView(text: String) {
+        this.findViewById<TextView>(R.id.tv_text3).setText(text)
+    }
+    //set text on the fourth TextView in the layout
+    fun setTextOnFourthTextView(text: String) {
+        this.findViewById<TextView>(R.id.tv_text4).setText(text)
+    }
+    //set text on all TextView's in the layout
+    fun setTextToAll(text: String, text2: String, text3: String, text4: String) {
+
+        this.findViewById<TextView>(R.id.tv_text1).setText(text)
+        this.findViewById<TextView>(R.id.tv_text2).setText(text2)
+        this.findViewById<TextView>(R.id.tv_text3).setText(text3)
+        this.findViewById<TextView>(R.id.tv_text4).setText(text4)
+    }
+
+    //Set text for CallButton
+    fun setCallButtonText(text: String) {
+        this.findViewById<MaterialButton>(R.id.btn_call).setText(text)
+    }
+
+    //Set ClickListener for CallButton
+    fun setCallButtonOnClickListener(listener: View.OnClickListener) {
+        this.findViewById<MaterialButton>(R.id.btn_call).setOnClickListener(listener)
+    }
+
+    //Set text for CancelButton
+    fun setCancelButtonText(text: String) {
+        this.findViewById<MaterialButton>(R.id.btn_cancel).setText(text)
+    }
+
+    //Set ClickListener for CancelButton
+    fun setCancelButtonOnClickListener(listener: View.OnClickListener) {
+        this.findViewById<MaterialButton>(R.id.btn_cancel).setOnClickListener(listener)
+    }
+
+    fun hideFirstTextView() {
+        this.findViewById<TextView>(R.id.tv_text1).visibility = View.GONE
+    }
+
+    fun hideSecondTextView() {
+        this.findViewById<TextView>(R.id.tv_text2).visibility = View.GONE
+    }
+
+    fun hideThirdTextView() {
+        this.findViewById<TextView>(R.id.tv_text3).visibility = View.GONE
+    }
+
+    fun hideFourthTextView() {
+        this.findViewById<TextView>(R.id.tv_text4).visibility = View.GONE
+    }
+
+    fun hideCallButton() {
+        this.findViewById<MaterialButton>(R.id.btn_call).visibility = View.GONE
+    }
+
+    fun hideCancelButton() {
+        this.findViewById<MaterialButton>(R.id.btn_cancel).visibility = View.GONE
+    }
+
+    fun showFirstTextView() {
+        this.findViewById<TextView>(R.id.tv_text1).visibility = View.VISIBLE
+    }
+
+    fun showSecondTextView() {
+        this.findViewById<TextView>(R.id.tv_text2).visibility = View.VISIBLE
+    }
+
+    fun showThirdTextView() {
+        this.findViewById<TextView>(R.id.tv_text3).visibility = View.VISIBLE
+    }
+
+    fun showFourthTextView() {
+        this.findViewById<TextView>(R.id.tv_text4).visibility = View.VISIBLE
+    }
+
+    fun showCallButton() {
+        this.findViewById<MaterialButton>(R.id.btn_call).visibility = View.VISIBLE
+    }
+
+    fun showCancelButton() {
+        this.findViewById<MaterialButton>(R.id.btn_cancel).visibility = View.VISIBLE
+    }
+
+    fun setCallButtonClickable(isClickable: Boolean) {
+        this.findViewById<MaterialButton>(R.id.btn_call).isClickable = isClickable
+    }
+
+    fun setCancelButtonClickable(isClickable: Boolean) {
+        this.findViewById<MaterialButton>(R.id.btn_cancel).isClickable = isClickable
+    }
+
+    fun setCallButtonIconResource(resourceID: Int) {
+        this.findViewById<MaterialButton>(R.id.btn_call).setIconResource(resourceID)
+    }
+
+    fun setCallButtonIconDrawable(drawable: Drawable?) {
+        this.findViewById<MaterialButton>(R.id.btn_call).icon = drawable
+    }
+
+    @SuppressLint("CutPasteId")
+    fun setImageSize(size: Int) {
+        val scale: Float = context.resources.displayMetrics.density
+        val size_px: Int = ((size * scale + 0.5f).toInt())
+        this.findViewById<RelativeLayout>(R.id.rl_image_root).layoutParams.width = size_px
+        this.findViewById<RelativeLayout>(R.id.rl_image_root).layoutParams.height = size_px
+        val cardView = this.findViewById<CardView>(R.id.cv_main_cardview)
+        val cv_params = (cardView.layoutParams as ViewGroup.MarginLayoutParams).apply { topMargin = size_px/2 }
+        cardView.layoutParams = cv_params
+        val ll_data = this.findViewById<LinearLayout>(R.id.ll_data)
+        val ll_params = (ll_data.layoutParams as ViewGroup.MarginLayoutParams).apply { topMargin = size_px/2 }
+        ll_data.layoutParams = ll_params
+    }
+
+    fun getFirstTextView(): TextView {
+        return this.findViewById(R.id.tv_text1)
+    }
+
+    fun getSecondTextView(): TextView {
+        return this.findViewById(R.id.tv_text2)
+    }
+
+    fun getThirdTextView(): TextView {
+        return this.findViewById(R.id.tv_text3)
+    }
+
+    fun getFourthTextView(): TextView {
+        return this.findViewById(R.id.tv_text4)
+    }
+
+    fun getCallButtonView(): MaterialButton {
+        return this.findViewById(R.id.btn_call)
+    }
+
+    fun getCancelButtonView(): MaterialButton {
+        return this.findViewById(R.id.btn_cancel)
+    }
 }
--- app/src/main/java/ba/droidev/cooldialog/MainActivity.kt
+++ app/src/main/java/ba/droidev/cooldialog/MainActivity.kt
@@ -1,13 +1,42 @@
 package ba.droidev.cooldialog
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.widget.AbsListView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.graphics.drawable.DrawableCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Test library here
+        val button: MaterialButton = findViewById<MaterialButton>(R.id.btn_open_dialog)
+        button.setOnClickListener{
+            val dialog: CoolDialog = CoolDialog(this)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.requestContentView()
+            val dialog_window = dialog.window
+            dialog_window?.setLayout(AbsListView.LayoutParams.MATCH_PARENT, AbsListView.LayoutParams.WRAP_CONTENT)
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.setCancelButtonOnClickListener(View.OnClickListener { dialog.dismiss() })
+            dialog.setCallButtonOnClickListener(View.OnClickListener { Toast.makeText(baseContext, "CALL BUTTON CLICKED", Toast.LENGTH_LONG).show() })
+            dialog.setTextToAll("Annie Fox", "annie.fox@droidev.com", "812-913-7452", "New Jersey, NY, USA")
+            dialog.setImageResource(R.drawable.profile_picture)
+            dialog.setImageSize(170)
+
+            val textView: TextView = dialog.getFirstTextView()
+            textView.textSize = 20f
+
+            dialog.show()
+        }
     }
 }
